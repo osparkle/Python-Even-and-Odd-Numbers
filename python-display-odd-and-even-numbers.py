12a53b6d88ce955951622424b2f43313f1c5b7ae
Simeon Adedokun <15355542+osparkle@users.noreply.github.com>
2020-11-29 09:24:19 +0100

    Add files via upload

---
+++ python-display-odd-and-even-numbers.py
@@ -0,0 +1,25 @@
+# ============================================
+# Python program to display
+# Odd and Even Numbers 
+# between 1 and 30
+# Author: Simeon Adedokun
+# Date: July 2, 2020
+# Email: ayoade@simdols.com
+# Website: https://educlancy.blogspot.com
+# ============================================
+
+last_number = 30 #the end number - value can be changed
+
+# Display Even Number
+print("Even Numbers:")
+i = 2 #initial even number
+while i <= last_number:
+ 	print(i),
+  	i += 2 #increment i by 2
+
+#Display Odd Numbers
+print("\n\nOdd Numbers:")
+i = 1 #initial odd number
+while i <= last_number:
+	print(i),
+	i += 2 #increment i by 2
